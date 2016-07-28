88b017ec417e0d2a173a1c82bc2381c74bace566
SyedAmirHussain <huss0104@hotmail.com>
2016-07-28 21:50:19 +1000

    uploaded assignment 2 cachematrix

---
+++ cacheMatrix_Syed.R
@@ -0,0 +1,35 @@
+makeCacheMatrix <- function(x = matrix()) {
+  
+  inv = NULL
+  set = function(y) {
+ 
+    x <<- y
+    inv <<- NULL
+  }
+  get = function() x
+  setinv = function(inverse) inv <<- inverse 
+  getinv = function() inv
+  list(set=set, get=get, setinv=setinv, getinv=getinv)
+}
+
+cacheSolved <- function(x, ...) {
+  
+  inv = x$getinv()
+  
+  # if the inverse has already been calculated
+  if (!is.null(inv)){
+    # get it from the cache and skips the computation. 
+    message("pulling cached data")
+    return(inv)
+  }
+  
+  # otherwise, calculates the inverse 
+  mat.data = x$get()
+  inv = solve(mat.data, ...)
+  
+  # sets the value of the inverse in the cache via the setinv function.
+  x$setinv(inv)
+  
+  return(inv)
+}
+
